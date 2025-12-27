162e13519b34c7dc9ea0c8d49147fee49fdfbb51
Talha Sialvi <mtalhakhansialvi786@gmail.com>
2025-12-27 13:16:26 +0500

    Delete models directory

--- models/Feedback.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const FeedbackSchema = new mongoose.Schema({
-    userName: String,
-    userEmail: String,
-    rating: Number,
-    message: String,
-}, { timestamps: true });
-module.exports = mongoose.model('Feedback', FeedbackSchema);
