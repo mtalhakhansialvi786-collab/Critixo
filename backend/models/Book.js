c441b04bb7e58e7068ce1804315772e85335380e
Talha Sialvi <mtalhakhansialvi786@gmail.com>
2025-12-30 01:32:23 +0500

    Update Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -4,7 +4,7 @@ const BookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true }, // Admin form mein ye hona lazmi tha
     price: { type: Number, required: true },
-    language: { type: String, enum: ['Urdu', 'English'], default: 'English' },
+    language: { type: String, enum: ['Urdu', 'English'], default: 'Urdu' },
     image: { type: String, required: true },
     category: { type: String },
     reviews: { type: String },
